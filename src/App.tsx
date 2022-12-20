8fa0a5056f8b98132c712f1da4867bf0e1b7ba12
Олег Болтунов <boltunovov@akbarsdigital.ru>
2022-12-20 23:09:00 +0300

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,11 @@
-import React, { useEffect, useState } from "react"
-import logo from "./logo.svg"
-import "./App.css"
-
-import { FrontendApi, Configuration, Session, Identity } from "@ory/client"
-
-const basePath = process.env.REACT_APP_ORY_URL;
-const ory = new FrontendApi(
-    new Configuration({
-        basePath,
-        baseOptions: {
-            withCredentials: true,
-        },
-    }),
-)
+import React from 'react';
 
 function App() {
-    const [session, setSession] = useState<Session | undefined>();
-    const [logoutUrl, setLogoutUrl] = useState<string | undefined>()
-
     return (
         <div className="App">
             <header className="App-header">
                 <p>
-                    Edit <code>src/App.js</code> and save to reload.
+                    Edit <code>src/App.tsx</code> and save to reload.
                 </p>
                 <a
                     className="App-link"
