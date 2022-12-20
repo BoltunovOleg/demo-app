ffe6b9a23da9c0a7f8c8da02c1c2633c72ddd0cb
Олег Болтунов <boltunovov@akbarsdigital.ru>
2022-12-20 23:08:32 +0300

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,23 @@
+import React, { useEffect, useState } from "react"
+import logo from "./logo.svg"
+import "./App.css"
+
+import { FrontendApi, Configuration, Session, Identity } from "@ory/client"
+
+const basePath = process.env.REACT_APP_ORY_URL;
+const ory = new FrontendApi(
+    new Configuration({
+        basePath,
+        baseOptions: {
+            withCredentials: true,
+        },
+    }),
+)
+
 function App() {
+    const [session, setSession] = useState<Session | undefined>();
+    const [logoutUrl, setLogoutUrl] = useState<string | undefined>()
+
     return (
         <div className="App">
             <header className="App-header">
